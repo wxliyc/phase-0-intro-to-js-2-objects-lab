8653107a283bcbbac57c7e4f671ecdf556f502b9
Ethan Lee <wxliyc@gmail.com>
2022-02-25 09:34:14 -0600

    commit tree 403b7144a969236b67ad6c93268aa3e3e09805ae

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: "",
+    streetAddress: "",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee};
+    newObj[key] = value; 
+return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+return employee; 
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObject = {...employee};
+    delete newObject.name, newObject.streetAddress;
+return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name, employee.streetAddress;
+return employee;
+}
